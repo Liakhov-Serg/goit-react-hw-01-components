10b36d2015e4fa4e774575d9238b3d9840a2b9f1
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-01-28 23:25:01 +0200

    Update App.jsx
    23-24

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,12 @@
 import { Profile } from "./Profile/Profile";
+import { Statistics } from "./Statistics/Statistics";
+import { FriendList } from "./FriendList/FriendList";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
 
+import user from "./Profile/user.json";
+import data from "./Statistics/data.json";
+import friends from "./FriendList/friends.json";
+import transactions from "./TransactionHistory/transactions.json";
 
 export const App = () => {
   return (
@@ -13,7 +20,16 @@ export const App = () => {
       //   color: '#010101'
       // }}
     >
-      <Profile />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
       React homework template
     </div>
   );
