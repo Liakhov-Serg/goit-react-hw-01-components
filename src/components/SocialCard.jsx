29af9055f9b665accc6566b2a22009b1a1a53d85
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-01-28 16:27:40 +0200

    saturday
    16-24

---
+++ src/components/SocialCard.jsx
@@ -0,0 +1,17 @@
+function SocialCard(props) {
+    
+}
+
+function App() {
+    return (
+        <div>
+            <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
+        </div>
+    )
+}
