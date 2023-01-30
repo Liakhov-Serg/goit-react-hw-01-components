14c57cb5087c79eb1feaf3c32e8a13923152a377
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-01-30 16:16:41 +0200

    Monday
    16-15

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import { Profile } from "./Profile/Profile";
-import { Statistics } from "./Statistics/Statistics";
-import { FriendList } from "./FriendList/FriendList";
-import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+// import { Statistics } from "./Statistics/Statistics";
+// import { FriendList } from "./FriendList/FriendList";
+// import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
 
 import user from "./Profile/user.json";
-import data from "./Statistics/data.json";
-import friends from "./FriendList/friends.json";
-import transactions from "./TransactionHistory/transactions.json";
+// import data from "./Statistics/data.json";
+// import friends from "./FriendList/friends.json";
+// import transactions from "./TransactionHistory/transactions.json";
 
 export const App = () => {
   return (
@@ -26,10 +26,10 @@ export const App = () => {
         location={user.location}
         avatar={user.avatar}
         stats={user.stats} />
-      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-      React homework template
+      <TransactionHistory items={transactions} /> */}
+      {/* <p className={scc.h1}>React homework template</p>  */}
     </div>
   );
 };
