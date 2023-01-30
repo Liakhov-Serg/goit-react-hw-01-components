bbd22d12417a9d893b00a2abdc2dfe38abc57529
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-01-30 18:58:03 +0200

    19-00
    Profile, FriendsList good

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import { Profile } from "./Profile/Profile";
 // import { Statistics } from "./Statistics/Statistics";
-// import { FriendList } from "./FriendList/FriendList";
+import { FriendList } from "./FriendList/FriendList";
 // import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
 
 import user from "./Data/user.json";
-// import data from "./Statistics/data.json";
-// import friends from "./FriendList/friends.json";
-// import transactions from "./TransactionHistory/transactions.json";
+// import data from "./Data/data.json";
+import friends from "./Data/friends.json";
+// import transactions from "./Data/transactions.json";
 
 export const App = () => {
   return (
@@ -26,8 +26,9 @@ export const App = () => {
         location={user.location}
         avatar={user.avatar}
         stats={user.stats} />
-      {/* <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      {/* <Statistics title="Upload stats" stats={data} />
+      
       <TransactionHistory items={transactions} /> */}
       {/* <p className={scc.h1}>React homework template</p>  */}
     </div>
