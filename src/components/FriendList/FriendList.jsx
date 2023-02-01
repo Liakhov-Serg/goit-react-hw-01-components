54a168b2bd2d9c0ff407649c1a8c87d3428395d1
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-02-01 19:21:26 +0200

    Update FriendList.jsx
    19-20

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -20,8 +20,10 @@ export const FriendList = ({ friends }) => {
 };
 
 FriendList.propTypes = {
+  friends: PropTypes.arrayOf(PropTypes.shape({
   avatar: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   isOnline: PropTypes.bool.isRequired,
-  id: PropTypes.number.isRequired,
+    id: PropTypes.number.isRequired,
+  }))
 };
