8b226a10bca4d95343212207ea3900d7b6088bd7
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-02-01 19:09:41 +0200

    Update Statistics.jsx
    111

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -42,5 +42,9 @@ export const Statistics = ({ title, stats }) => {
 
 Statistics.propTypes = {
     title: PropTypes.string.isRequired,
-    stats: PropTypes.number.isRequired,
+    stats: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired,
+    }))
 };
