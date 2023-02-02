c7a3aa22fab74c4aef35d04bd3c8d0617478ba89
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-02-02 14:24:36 +0200

    111
    111

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,5 +1,7 @@
 import css from './FriendList.module.css';
 import PropTypes from 'prop-types';
+import { FriendListItem } from './FriendListItem/FriendListItem';
+
 
 export const FriendList = ({ friends }) => {
     return (
--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,14 +1,21 @@
 import css from './FriendListItem/FriendListItem.module.css';
 import PropTypes from 'prop-types';
 
-export const FriendListItem = ({avatar, name, isOnline }) => {
+export const FriendListItem = ({ avatar, name, isOnline, id }) => {
     return (
         <li className={css.friends__item} key={id}>
             <span className={`${css.friends__status} ${css[isOnline]}`}>
-              {isOnline}
+                {isOnline}
             </span>
             <img className={css.avatar} src={avatar} alt={name} />
             <p className={css.friends__name}>{name}</p>
-          </li>
+        </li>
     )
+};
+
+FriendListItem.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+    id: PropTypes.number.isRequired,
 }
